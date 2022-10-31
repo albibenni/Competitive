e696da37ff589b200800553dee01238fae1629ef
Alberto Benatti <albi.benni8@gmail.com>
2022-10-31 11:59:24 +0100

    tipletSum - set answer concise

--- src/main/java/org/albi/TripletSum.java
+++ src/main/java/org/albi/TripletSum.java
@@ -2,10 +2,7 @@ package org.albi;
 
 import lombok.Builder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Builder
 public class TripletSum {
@@ -20,62 +17,48 @@ public class TripletSum {
     Notice that the order of the output and the order of the triplets does not matter.
     */
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length < 3) return Collections.emptyList();
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
-        int i, j, z, a, b, c, iLength, jLength;
+        int a;
+        int b;
+        int c;
         int numLength = nums.length;
-        iLength = (numLength / 3);
-        jLength = iLength * 2;
-        if (nums.length == 3) {
+        if (numLength < 3) return Collections.emptyList();
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        if (numLength == 3) {
             a = nums[0];
             b = nums[1];
             c = nums[2];
-            if (tripletSumEqualZero(a, b, c)) {
+            if (a+b+c == 0) {
                 result.add(List.of(a, b, c));
             }
-            return result;
+            return new ArrayList<>(result);
         }
-        for (i = 0; i < iLength; i++) {
+        for (int i = 0; i < numLength - 2; i++) {
             a = nums[i];
-            for (j = iLength; j < jLength; j++) {
-                b = nums[j];
-                for (z = jLength; z < numLength; z++) {
-                    c = nums[z];
-                    if (tripletSumEqualZero(a, b, c)) {
-                        result.add(List.of(a, b, c));
-                    }
-                }
+            int j = i + 1;
+            int k = numLength - 1;
+            while (j < k) {
+                int sum = a + nums[j] + nums[k];
+                if (sum == 0) result.add(Arrays.asList(a, nums[j++], nums[k--]));
+                else if (sum > 0) k--;
+                else j++;
             }
         }
-        return checkTripletsDuplicate(result);
-    }
-
-    private boolean tripletSumEqualZero(int a, int b, int c) {
-        return a + b + c == 0;
-    }
-
-    private List<List<Integer>> checkTripletsDuplicate (List<List<Integer>> checkTriplets) {
-        for (int listIndex = 0; listIndex < checkTriplets.size(); listIndex++) {
-            for (int nextIndex = listIndex; nextIndex < checkTriplets.size(); nextIndex++) {
-                if (checkTriplets.get(listIndex).equals(checkTriplets.get(nextIndex))){
-                    checkTriplets.remove(nextIndex);
-                }
-            }
-        }
-
-        return checkTriplets;
+        return new ArrayList<>(result);
     }
 
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
         int[] nums2 = {0, 1, 1};
         int[] nums3 = {0, 0, 0};
+        int[] nums4 = {1, -1, -1, 0};
         List<List<Integer>> lists = TripletSum.builder().build().threeSum(nums);
         List<List<Integer>> lists2 = TripletSum.builder().build().threeSum(nums2);
         List<List<Integer>> lists3 = TripletSum.builder().build().threeSum(nums3);
+        List<List<Integer>> lists4 = TripletSum.builder().build().threeSum(nums4);
         System.out.println(lists);
         System.out.println(lists2);
         System.out.println(lists3);
+        System.out.println(lists4);
     }
 }
