70f029a866518a7d31ba31e7a84449b694cf7729
Alberto Benatti <albi.benni8@gmail.com>
2022-11-02 20:52:52 +0100

    image overlap - w/ 36ms run

--- src/main/java/org/albi/imageOverlap/ImageOverlap.java
+++ src/main/java/org/albi/imageOverlap/ImageOverlap.java
@@ -30,12 +30,13 @@ public class ImageOverlap {
 
         //Slide one image on other image.
         for(int row=0; row<row1+row2-1; row++){
-            //index to iterate on the two images
+            //index to iterate on the two images rows
             int i = row1-1 + Math.min(0, row2-1-row);
             int x = row + Math.min(0, row2-1-row);
-            for(int c=0; c<column1+column2-1;  c++){
-                int j = column1-1 + Math.min( 0 ,  column2-1-c);
-                int y = c + Math.min(0 , column2-1-c);
+            for(int column=0; column<column1+column2-1;  column++){
+                //index to iterate on the two images columns
+                int j = column1-1 + Math.min( 0 ,  column2-1-column);
+                int y = column + Math.min(0 , column2-1-column);
                 max = Math.max(max, overlap(img1,img2,i,j,x,y));
             }
         }
@@ -45,6 +46,7 @@ public class ImageOverlap {
     //Compares two matrices from give coordinates to LEFT-TOP
     int overlap(int[][] img1, int[][] img2, int i1,int j1, int i2, int j2){
         int count = 0;
+        //double for loop to check 2 images
         for(int i=i1,x=i2; i>=0 && x>=0; i--,x--){
             for(int j=j1, y=j2; j >=0 && y>=0 ; j--,y--){
                 if(img1[i][j] == img2[x][y]){
@@ -56,11 +58,75 @@ public class ImageOverlap {
         //System.out.println("[" + i1+" , "+j1+"]    ["+ i2 + " , "+j2+" ]" + "===" + count);
         return count;
     }
+
+
     public static void main(String[] args) {
         int[][] img1 = {{1,1,0}, {0, 1, 0}, {0, 1, 0}};
         int[][] img = {{0,0,0}, {0, 1, 1}, {0, 0, 1}};
         ImageOverlap imageOverlap = ImageOverlap.builder().build();
         int overlap = imageOverlap.largestOverlap(img1, img);
         System.out.println(overlap);
+    }
+}
+
+//36ms run
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            int row = 0;
+            for (int j = 0; j < n ; j++) {
+                row = (row << 1) + img1[i][j];
+            }
+            arr1[i] = row;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int row = 0;
+            for (int j = 0; j < n ; j++) {
+                row = (row << 1) + img2[i][j];
+            }
+            arr2[i] = row;
+        }
+
+        int ans = 0;
+        for (int i = 1-n; i <= n-1; i++) { //shift horizontally, negative means left, positive means right
+            for (int j = 1-n; j <= n-1; j++) { //shift vertically, negative means up, positive means down
+                //if scroll 0, row 0 - n-1
+                //if scroll up by n-1, row n-1, if j , then abs(j) - n-1
+                //if scroll down by n-1, row 0 - j, then 0 - n-j
+                int count = 0;
+                if (j <= 0) { // go up
+                    if (i <= 0) {
+                        //arr1 [abs(j)~n-1] & arr2 [abs(j)~n-1] and count(1)
+                        for (int k = -j ; k < n; k++) {
+                            count +=Integer.bitCount((arr1[k] << (-i)) & arr2[k+j]);
+                        }
+                    }
+                    else {
+                        for (int k = -j ; k < n; k++) {
+                            count +=Integer.bitCount((arr1[k] >> i) & arr2[k+j]);
+                        }
+                    }
+                }
+                else { //go down
+                    if (i <= 0) {
+                        //arr1 [abs(j)~n-1] & arr2 [abs(j)~n-1] and count(1)
+                        for (int k = 0 ; k < n-j; k++) {
+                            count +=Integer.bitCount((arr1[k] << (-i)) & arr2[j+k]);
+                        }
+                    }
+                    else {
+                        for (int k = 0 ; k < n-j; k++) {
+                            count +=Integer.bitCount((arr1[k] >> i) & arr2[j+k]);
+                        }
+                    }
+                }
+                ans = Math.max(ans, count);
+            }
+        }
+        return ans;
     }
 }
