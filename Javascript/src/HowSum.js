ff1166f7386f547a3a3049f1a07424a3ed3380ec
Alberto Benatti <albi.benni8@gmail.com>
2023-02-11 15:33:29 +0100

    java is trash

--- Javascript/src/HowSum.js
+++ Javascript/src/HowSum.js
@@ -1,32 +1,49 @@
-//howSum takes in targetSum and an array of numbers as arguments
+//howSumBruteForce takes in targetSum and an array of numbers as arguments
 //should return an array containing any combination of elements
 // that add up to exactly to targetSum else null
 
-const howSum = (targetSum, numbers) => {
+const howSumBruteForce = (targetSum, numbers) => {
     if (targetSum === 0) return [];
     if (targetSum < 0) return null;
     for (let number of numbers) {
         const remainder = targetSum - number;
-        const remainderResult = howSum(remainder, numbers);
+        const remainderResult = howSumBruteForce(remainder, numbers);
         if (remainderResult !== null) {
             return [...remainderResult, number];
         }
     }
     return null;
 }
+//Space C: O(m+m)
+// Time: O(m*n^m)
 
-const numbers1 = [2, 3];
-const numbers10 = [5, 3, 4, 7];
-const numbers2 = [2, 4];
-const numbers3 = [2, 3, 5];
-// const numbers4 = [7, 14];
-// const result = howSum(7, numbers1);
-// const result2 = howSum(7, numbers10);
-// const result3 = howSum(7, numbers2);
-// const result4 = howSum(8, numbers3);
-// const result5 = howSum(300, numbers4);
-console.log(howSum(7, [2, 3]));
-console.log(howSum(7, [5, 3, 4, 7]));
-console.log(howSum(7, [2, 4]));
-console.log(howSum(8, [2, 3, 5]));
-// console.log(howSum(300, [7, 14]));
+
+const howSumMemo = (targetSum, numbers, memo= {}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+    for (let number of numbers) {
+        const remainder = targetSum - number;
+        const remainderResult = howSumMemo(remainder, numbers, memo);
+        if (remainderResult !== null) {
+            memo[targetSum] = [...remainderResult, number];
+            return memo[targetSum];
+        }
+    }
+    memo[targetSum] = null;
+    return null;
+}
+
+// Space: O(m*m)
+// Time: O(n*m*m)
+
+// console.log(howSumBruteForce(7, [2, 3]));
+// console.log(howSumBruteForce(7, [5, 3, 4, 7]));
+// console.log(howSumBruteForce(7, [2, 4]));
+// console.log(howSumBruteForce(8, [2, 3, 5]));
+// console.log(howSumBruteForce(300, [7, 14]));
+console.log(howSumMemo(7, [2, 3]));
+console.log(howSumMemo(7, [5, 3, 4, 7]));
+console.log(howSumMemo(7, [2, 4]));
+console.log(howSumMemo(8, [2, 3, 5]));
+console.log(howSumMemo(300, [7, 14]));
