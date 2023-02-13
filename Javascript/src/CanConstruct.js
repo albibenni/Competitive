6c19f50fcd307be1d7094549aa4f2fbee5f3d582
Alberto Benatti <albi.benni8@gmail.com>
2023-02-13 09:20:59 +0100

    memoized CanConstruct.js

---
+++ Javascript/src/CanConstruct.js
@@ -0,0 +1,53 @@
+/*
+Write a function `canConstruct(target, wordBank)` that accepts a target string and an array of strings.
+
+The function should return a boolean indicating whether or not the `target` can be constructed by concatenating 
+elements of the `wordBank` array.
+
+You may reuse elements of `wordBank` as many times as needed.
+ */
+
+const canConstructMemo = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target]
+    if (target === "") return true;
+
+    for (let word of wordBank) {
+        //     check if prefix
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            memo[target] = suffix;
+            if (canConstructMemo(suffix, wordBank, memo) === true) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const canConstructBruteForce = (target, wordBank) => {
+    if (target === "") return true;
+
+    for (let word of wordBank) {
+        //     check if prefix
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            if (canConstructBruteForce(suffix, wordBank) === true) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+// console.log(canConstructBruteForce("skataboard", ["bo", "rd", "ate", "t", "ska", "boar"])); // ->false
+// console.log(canConstructBruteForce("", ["bo", "rd", "ate", "t", "ska", "boar"])); // -> true
+// console.log(canConstructBruteForce("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // -> true
+// console.log(canConstructBruteForce("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "ot"])); // -> true
+// console.log(canConstructBruteForce("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee","eeeee", "eeeeee"])); // -> false
+
+console.log(canConstructMemo("skataboard", ["bo", "rd", "ate", "t", "ska", "boar"])); // ->false
+console.log(canConstructMemo("", ["bo", "rd", "ate", "t", "ska", "boar"])); // -> true
+console.log(canConstructMemo("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // -> true
+console.log(canConstructMemo("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "ot"])); // -> true
+console.log(canConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee","eeeee", "eeeeee"])); // -> false
