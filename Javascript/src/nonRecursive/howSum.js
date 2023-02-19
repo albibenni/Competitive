ec936b3123fa2fef4540c4c128090e938f4a036f
Alberto Benatti <albi.benni8@gmail.com>
2023-02-19 14:33:43 +0100

    tabulation how sum

--- Javascript/src/nonRecursive/howSum.js
+++ Javascript/src/nonRecursive/howSum.js
@@ -25,7 +25,7 @@ const howSum = (targetSum, numbers) => {
 // Space: O(m*m )
 // Time: O(m*m*n)
 console.log(howSum(7, [2, 3]));
-// console.log(howSum(7, [5, 3, 4, 7]));
-// console.log(howSum(7, [2, 4]));
-// console.log(howSum(8, [2, 3, 5]));
-// console.log(howSum(300, [7, 14]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(300, [7, 14]));
