28e0dbcc6d92278488886d19b4a7964361bcef11
Alberto Benatti <albi.benni8@gmail.com>
2023-02-22 21:58:49 +0100

    tabulation CanConstruct.js

--- Javascript/src/nonRecursive/CanConstruct.js
+++ Javascript/src/nonRecursive/CanConstruct.js
@@ -8,21 +8,29 @@ You may reuse elements of `wordBank` as many times as needed.
  */
 
 const canConstruct = (target, wordBank) => {
-    if (target === "") return true;
+    const table = Array(target.length + 1).fill(false);
+    table[0] = true;
 
-    for (let word of wordBank) {
-        //     check if prefix
-        if (target.indexOf(word) === 0) {
-            const suffix = target.slice(word.length);
-            if (canConstructBruteForce(suffix, wordBank) === true) {
-                return true;
+    for (let i = 0; i <= target.length; i++) {
+        if (table[i] === true) {
+            for (const word of wordBank) {
+                if (target.slice(i, i + word.length) === word) {
+                    const wordConcatLength = i + word.length;
+                    table[wordConcatLength] = true;
+                }
             }
         }
     }
-    return false;
+
+    return table[target.length];
 }
-console.log(canConstructMemo("skataboard", ["bo", "rd", "ate", "t", "ska", "boar"])); // ->false
-console.log(canConstructMemo("", ["bo", "rd", "ate", "t", "ska", "boar"])); // -> true
-console.log(canConstructMemo("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // -> true
-console.log(canConstructMemo("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "ot"])); // -> true
-console.log(canConstructMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee","eeeee", "eeeeee"])); // -> false
+
+/*
+// Space: O(m*m*n)
+// Time: O(m)
+ */
+console.log(canConstruct("skataboard", ["bo", "rd", "ate", "t", "ska", "boar"])); // ->false
+console.log(canConstruct("", ["bo", "rd", "ate", "t", "ska", "boar"])); // -> true
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // -> true
+console.log(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "ot"])); // -> true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])); // -> false
