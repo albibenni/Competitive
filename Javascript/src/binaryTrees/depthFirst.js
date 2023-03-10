2112875bde50f6fc8af24a490b55afea5de3d97c
Alberto Benatti <albi.benni8@gmail.com>
2023-03-10 22:45:14 +0100

    wip depthFirst.js

---
+++ Javascript/src/binaryTrees/depthFirst.js
@@ -0,0 +1,37 @@
+/*
+Time O(n)
+Space O(n)
+ */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+const depthFirstIT = (root) => {
+    const stack = [root];
+    while (stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+        if (current.right() !== null) stack.push(current.right());
+        if (current.left() !== null) stack.push(current.left());
+    }
+}
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+
+depthFirstIT(a);
