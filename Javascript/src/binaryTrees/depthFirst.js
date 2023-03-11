972b178ac532e9350bcfec69ab9a22f885aadb8a
Alberto Benatti <albi.benni8@gmail.com>
2023-03-11 15:22:19 +0100

    return value and null case

--- Javascript/src/binaryTrees/depthFirst.js
+++ Javascript/src/binaryTrees/depthFirst.js
@@ -10,14 +10,18 @@ class Node {
         this.right = null;
     }
 }
+
 const depthFirstIT = (root) => {
+    if (root === null) return [];
+    const result = []
     const stack = [root];
     while (stack.length > 0) {
         const current = stack.pop();
-        console.log(current.val);
+        result.push(current.val);
         if (current.right) stack.push(current.right);
         if (current.left) stack.push(current.left);
     }
+    return result;
 }
 
 const a = new Node('a');
