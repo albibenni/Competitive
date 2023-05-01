9dc4f73bece184490d76a6b6b21c58dcec332fc0
Alberto Benatti <albi.benni8@gmail.com>
2023-05-01 12:42:07 +0200

    static impl of binarySearch Java

---
+++ src/main/java/org/albi/algoFolder/fullAlgorithms/binaryTree/BinaryTree.java
@@ -0,0 +1,18 @@
+package org.albi.algoFolder.fullAlgorithms.binaryTree;
+
+public class BinaryTree {
+    private BinaryTree() {
+
+    }
+
+    public static int binarySearch(int[] a, int key) {
+        int lo = 0, hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+}
