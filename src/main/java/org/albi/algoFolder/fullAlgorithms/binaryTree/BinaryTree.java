72579e6175182456a3ffe03a56734b3a522d4b35
Alberto Benatti <albi.benni8@gmail.com>
2023-05-01 22:21:26 +0200

    bitonic search try

--- src/main/java/org/albi/algoFolder/fullAlgorithms/binaryTree/BinaryTree.java
+++ src/main/java/org/albi/algoFolder/fullAlgorithms/binaryTree/BinaryTree.java
@@ -15,4 +15,29 @@ public class BinaryTree {
         }
         return -1;
     }
+
+    boolean bitonicArraySearch(int[] a, int value) {
+        int[] asc = new int[a.length / 2];
+        int[] desc = new int[a.length / 2];
+        for (int i = 0; i < a.length / 2; i++) asc[i] = a[i];
+        if (a.length % 2 != 0) {
+            if (a[(a.length / 2)] == value) return true;
+            for (int i = (a.length / 2) + 1; i < a.length; i++) {
+                desc = new int[(a.length / 2) - 1];
+                desc[i] = a[i];
+            }
+        } else {
+            for (int i = a.length / 2; i < a.length; i++) {
+                desc[i] = a[i];
+            }
+        }
+        if (binarySearch(asc, value) != -1) return true;
+        if (binarySearch(desc, value) != -1) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(200 % 2 == 0);
+        System.out.println(3 / 2);
+    }
 }
