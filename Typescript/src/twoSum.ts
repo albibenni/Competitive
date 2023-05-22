3263afe93c3bb824e4dac673006c001f9f262560
Alberto Benatti <albi.benni8@gmail.com>
2023-05-22 16:26:34 +0200

    wip ts

--- Typescript/src/twoSum.ts
+++ Typescript/src/twoSum.ts
@@ -1,5 +1,8 @@
-const twoSum = (arr: number[]): number | null => {
+export function twoSum(arr: number[], sum: number): number | null {
+  arr.map(n1 => {
+    return arr.find(n2 => n2 + n1 === sum);
+  });
   return null;
-};
+}
 
-console.log(twoSum([1, 2, 3]));
+console.log(twoSum([1, 2, 3], 5));
