4bbb886ebf29049f3d0a03f41294f209857a1066
Alberto Benatti <albi.benni8@gmail.com>
2023-05-23 09:17:39 +0200

    leetcode request

--- Typescript/src/twoSumCustomMap.ts
+++ Typescript/src/twoSumCustomMap.ts
@@ -21,6 +21,13 @@ console.log(twoSumCustomMap([1, 3, 7, 9, 2], 11));
 console.log(twoSumCustomMap([], 5));
 
 // Leetcode
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+ */
 function twoSum(nums: number[], target: number): number[] | null {
   for (let i = 0; i < nums.length; i++) {
     const numToFind = target - nums[i];
