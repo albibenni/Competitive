92e951227bb9d9e30f74eaa258c907c76b9d2c3f
Alberto Benatti <albi.benni8@gmail.com>
2023-05-23 08:56:41 +0200

    brute force two sum

--- Typescript/src/twoSum.ts
+++ Typescript/src/twoSum.ts
@@ -2,8 +2,9 @@ export function twoSum(arr: number[], sum: number): Map<number, number> | null {
   const index: Map<number, number> = new Map();
   //return the index
   for (let i = 0; i < arr.length; i++) {
+    const numToFind = sum - arr[i];
     for (let j = i; j < arr.length; j++) {
-      arr[i] + arr[j] === sum && index.set(i, j);
+      arr[j] === numToFind && index.set(i, j);
     }
   }
   if (index.size === 0) return null;
