56d6560b9adddcabc41f0252fd59a64cf79b820d
Alberto Benatti <albi.benni8@gmail.com>
2023-05-27 18:44:48 +0200

    wip bruteforce

--- Typescript/src/medium/MostWater.ts
+++ Typescript/src/medium/MostWater.ts
@@ -1,1 +1,13 @@
-function maxArea(height: number[]): number {}
+function maxArea(height: number[]): number {
+  let result = 0;
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      const lowNumber = height[j] > height[i] ? height[i] : height[j];
+      const currArea = (j - i) * lowNumber;
+      if (currArea > result) result = currArea;
+    }
+  }
+  return result;
+}
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
