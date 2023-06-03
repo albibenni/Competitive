29cdc407437d9e26435a56f59813b79e3640fb78
Alberto Benatti <albi.benni8@gmail.com>
2023-06-03 09:38:39 +0200

    bruteforce

--- Typescript/src/medium/MostWater.ts
+++ Typescript/src/medium/MostWater.ts
@@ -1,10 +1,18 @@
+/*
+You are given an integer array height of length n.
+There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+ */
+
 function maxArea(height: number[]): number {
   let result = 0;
   for (let i = 0; i < height.length; i++) {
     for (let j = i + 1; j < height.length; j++) {
-      const lowNumber = height[j] > height[i] ? height[i] : height[j];
+      const lowNumber = Math.min(height[i], height[j]);
       const currArea = (j - i) * lowNumber;
-      if (currArea > result) result = currArea;
+      result = Math.max(currArea, result);
     }
   }
   return result;
