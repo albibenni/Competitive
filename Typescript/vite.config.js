945ed1cf0b5da5782524226c41e52499d6ce13f1
Alberto Benatti <albi.benni8@gmail.com>
2024-04-05 14:03:27 +0200

    Keep calm and commit

--- Typescript/vite.config.js
+++ Typescript/vite.config.js
@@ -1,11 +1,14 @@
 // vite.config.js
+import { glsl } from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 import _config from "./_config.js";
+import { VitePluginNode } from 'vite-plugin-node';
 
 const HOST = _config.server.host;
 const PORT = _config.server.port;
 
 export default defineConfig({
+  plugins: [ VitePluginNode(), glsl()],
   server: {
     host: HOST,
     port: PORT,
